ca40276b0e3b5c95e24d358ff1917bd372d9fa86
wakiwiki <wakiwiki@live.com>
2022-08-13 23:17:51 +0430

    code coverage

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -14,13 +14,16 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                var itemName = Items[i].Name;
+                if (itemName != "Aged Brie" && itemName != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (itemName != "Sulfuras, Hand of Ragnaros")
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            Items[i].Quality -= 1;
+                            if(Items[i].Quality > 0 && itemName == "Conjured Mana Cake")
+                                Items[i].Quality -= 1;
                         }
                     }
                 }
@@ -28,15 +31,15 @@ namespace GildedRoseKata
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality += 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (itemName == "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].SellIn < 11)
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
 
@@ -44,42 +47,42 @@ namespace GildedRoseKata
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (itemName != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Items[i].SellIn -= 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (itemName != "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (itemName != "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (itemName != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    Items[i].Quality -= 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality -= Items[i].Quality;
                         }
                     }
                     else
                     {
                         if (Items[i].Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].Quality += 1;
                         }
                     }
                 }
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using System.Collections.Generic;
+using FluentAssertions;
 using GildedRoseKata;
 
 namespace GildedRoseTests
@@ -12,7 +13,140 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Items[0].Name.Should().Be("foo");
+            //Assert.Equal("foo", Items[0].Name);
         }
+
+        [Fact]
+        public void degrade_quality_once_when_one_day_passed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal item", SellIn = 2, Quality = 4 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(3);
+        }
+
+        [Fact]
+        public void degrade_quality_twice_as_fast_when_sell_by_date_has_passed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal item", SellIn = -1, Quality = 4 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(2);
+        }
+
+        [Fact]
+        public void degrade_quality_should_keep_it_positive()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal item", SellIn = 1, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().BeGreaterThanOrEqualTo(0);
+        }
+
+        [Fact]
+        public void increase_quality_when_aged_brie_gets_older()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 4 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().BeGreaterThan(4);
+            Items[0].Quality.Should().Be(5);
+        }
+
+
+        [Fact]
+        public void aged_brie_quality_should_be_increased_twice__when_sell_by_date_has_passed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 48 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(50);
+        }
+
+        [Fact]
+        public void increase_quality_never_sets_more_than_fifty()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().BeLessThanOrEqualTo(50);
+            Items[0].Quality.Should().Be(50);
+        }
+
+        [Fact]
+        public void sulfuras_sellin_should_not_be_matter()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 2, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].SellIn.Should().Be(2);
+        }
+
+
+        [Fact]
+        public void sulfuras_quality_must_be_eighty()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 2, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(80);
+        }
+
+        [Theory()]
+        [InlineData(11)]
+        [InlineData(12)]
+        public void backstage_pass_quality_should_be_increased_once_before_sellin_gets_closer(int sellin)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellin, Quality = 30 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(31);
+        }
+
+        [Theory()]
+        [InlineData(10)]
+        [InlineData(8)]
+        [InlineData(6)]
+        public void backstage_pass_quality_should_be_increased_by_two_when_there_are_ten_days_or_less_for_sellin(int sellin)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellin, Quality = 30 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(32);
+        }
+
+
+        [Theory()]
+        [InlineData(5)]
+        [InlineData(3)]
+        [InlineData(1)]
+        public void backstage_pass_quality_should_be_increased_by_three_when_there_are_five_days_or_less_for_sellin(int sellin)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellin, Quality = 30 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(33);
+        }
+
+        [Fact]
+        public void backstage_pass_quality_should_be_set_to_zero_when_sell_by_date_has_passed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 30 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void conjured_item_quality_degrade_twice_as_fast_as_normal_item()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 2, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(18);
+        }
+
     }
 }
